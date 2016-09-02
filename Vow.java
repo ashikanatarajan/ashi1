119806929c1f4673d1d7ff305b83f07b8cdf3a50
ashikanatarajan <mail2me.aashi@gmail.com>
2016-09-02 08:04:38 +0530

    Create Vow.java

---
+++ Vow.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public Vow
+{
+Public static void main(String ar[])
+{
+Scanner sc=new Scanner(System.in);
+char a;
+a=sc.next.charAt(0);
+if(ch==a||ch==A||ch==e||ch==E||ch==i||ch==I||ch==o||ch==O||ch==u||ch==U)
+{
+System.out.println("it is vowel");
+}
+else
+System.out.println("it is consonant");
+}
+}
