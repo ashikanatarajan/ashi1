bd0e9dc2587d57a297562ff4e3db7dc7dc57108f
ashikanatarajan <mail2me.aashi@gmail.com>
2016-09-07 05:19:09 +0530

    Create its.java

---
+++ its.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class its
+{
+public static void main(String arr[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+int a=Integer.parseInt(s);
+System.out.print(a);
+}
+}
