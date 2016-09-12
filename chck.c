17d2bf3c468c530eda61e2984b870a1768c6b75b
ashikanatarajan <mail2me.aashi@gmail.com>
2016-09-12 12:19:15 +0530

    Update and rename chck.java to chck.c

---
+++ chck.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,x,p,m,k=1;
+printf("Enter the value:");
+scanf("%d",&n);
+printf("Enter the pair:");
+scanf("%d",&p);
+for(m=0;m<2*n;m++)
+{
+x=2*n-k;
+if(p!=x)
+{
+printf("The pair is%d for days %d is %d\n",p,j++,p);
+}
+}
+getch();
+}
